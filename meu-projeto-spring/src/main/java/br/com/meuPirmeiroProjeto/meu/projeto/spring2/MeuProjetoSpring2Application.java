1d57b73ced8f58f39ac1cae49c6dbdb35b47812f
Renan S Belchior <102825017+RenanBelchior@users.noreply.github.com>
2022-04-19 00:57:26 -0300

    Add files via upload

---
+++ meu-projeto-spring/src/main/java/br/com/meuPirmeiroProjeto/meu/projeto/spring2/MeuProjetoSpring2Application.java
@@ -0,0 +1,13 @@
+package br.com.meuPirmeiroProjeto.meu.projeto.spring2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuProjetoSpring2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuProjetoSpring2Application.class, args);
+	}
+
+}
---
+++ meu-projeto-spring/src/test/java/br/com/meuPirmeiroProjeto/meu/projeto/spring2/MeuProjetoSpring2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.meuPirmeiroProjeto.meu.projeto.spring2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuProjetoSpring2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
